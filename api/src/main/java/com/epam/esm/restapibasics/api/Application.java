55fc160a3425ea888d96be90160cdae61b0dbcac
prplcld <antonsilebin@mail.ru>
2022-01-12 17:19:24 +0300

    main fix

--- api/src/main/java/com/epam/esm/restapibasics/api/Application.java
+++ api/src/main/java/com/epam/esm/restapibasics/api/Application.java
@@ -3,13 +3,12 @@ package com.epam.esm.restapibasics.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.epam.esm.restapibasics"})
 @ComponentScan(basePackages = {"com.epam.esm.restapibasics"})
-public class Application extends SpringBootServletInitializer {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
